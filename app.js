4a9c203af7b58ecee8960dd4a77f7e176f1b9ae1
Anastasia Polivoda <polivoda.a@zovplita.by>
2022-06-01 21:57:02 +0300

    fix: alt attribute to img tags in added tasks according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="task-btn tasks-list__delete-btn";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="tasks-list__delete-img";
+    deleteButtonImg.setAttribute("alt", "Delete image");
     deleteButton.appendChild(deleteButtonImg);
 
 
